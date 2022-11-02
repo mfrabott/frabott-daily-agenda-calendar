721ff1450b416fefbf6f4b9a8c926d55d2b8ca7d
mfrabott <mfrabott14@gmail.com>
2022-11-02 10:07:06 -0400

    target id value and convert to number for comparison

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,33 @@ $(function () {
   // useful when saving the description in local storage?
   //
   // TODO: Add code to apply the past, present, or future class to each time
+  
+  // convert current hour to number for evaluation
+  var currentHour = Number(dayjs().format('HH'));
+  var agendaHoursEl = $('.agendaHours');
+  
+  hourCheck= agendaHoursEl.children().attr('id').match(/\d+/)[0];
+  console.log(hourCheck[0]);
+
+  
+  var hour = document.querySelector('.agendaHours');
+  console.log(hour);
+  var agendaHourId = parseFloat(hour.textContent);
+  
+  for (i=9; i<6; i++){
+    $("'hour-'i").addClass('future').removeClass('past present');
+    console.log($("'hour-'i"));
+  }
+
+  // if (agendaHourId === currentHour){
+  //   $('.agendaHours').children('div').eq(0).addClass('present').removeClass('past future');
+  // } else if (agendaHourId > currentHour){
+  //   $('.agendaHours').children('div').eq(0).addClass('future').removeClass('past present');
+  // } else {
+  //   $('.agendaHours').children('div').eq(0).addClass('past').removeClass('past future');
+  // }
+
+
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
@@ -22,5 +49,18 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
   var today = dayjs().format('dddd, MMMM D, YYYY');
   $('#currentDay').text(today);
+  var currentTime = dayjs().format('h:mm A');
+  $('#currentTime').text(currentTime);
+
+
+  
+
+  
 });
+
+
+
+
+
+
 
