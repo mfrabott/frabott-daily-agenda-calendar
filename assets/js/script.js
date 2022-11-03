ab85a54048516f68d35db5281bd767bcd58c2dbb
mfrabott <mfrabott14@gmail.com>
2022-11-03 19:21:13 -0400

    created function to look for existing key values and replace if so, otherwise push to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,8 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //  
+
+  // save buttons targeted for each hour
  	var saveButton9 = document.getElementById("9");
 	var saveButton10 = document.getElementById("10");
 	var saveButton11 = document.getElementById("11");
@@ -19,11 +14,9 @@ $(function () {
 	var saveButton16 = document.getElementById("16");
 	var saveButton17 = document.getElementById("17");
   var saveButton18 = document.getElementById("18");
-    
-  // var saveButtons = [saveButton9, saveButton10, saveButton11, saveButton12, saveButton13, saveButton14, saveButton15, saveButton16, saveButton17];
-
   var agendaEvents = [];
 
+  // Function to save user input into loval storage
   var saveEvent = function(buttonNumber){
     readEventsFromStorage();
     var hourBlock = buttonNumber.parentElement;
@@ -33,71 +26,20 @@ $(function () {
         hour : newEventHourId,
         event : newEventInput
     };
-
-    agendaEvents.push(newEvent);
+    
+    var indexOfObject = agendaEvents.findIndex(agendaEvents => {
+      return agendaEvents.hour === newEvent.hour;
+    });
+    console.log(indexOfObject);
+    if (indexOfObject < 0){
+      agendaEvents.push(newEvent);
+    } else {
+      agendaEvents.splice(indexOfObject, 1, newEvent);
+    };
+    
     localStorage.setItem('agendaEvents', JSON.stringify(agendaEvents))
-    readEventsFromStorage();
   };
 
-    // TODO: ARRAY AND FOR LOOP - review https://stackoverflow.com/questions/19586137/addeventlistener-using-for-loop-and-passing-values
-    // for(i=0; i<saveButtons.length; i++){
-    //     var buttonInit = saveButtons[i];
-    //     console.log(buttonInit.parentElement);
-    //     buttonInit.addEventListener('click', function (event) {
-    //         // buttonInit = saveButtons[i];
-    //         var hourBlock = buttonInit.parentElement;
-    //         console.log(hourBlock);
-    //         var newEventInput = hourBlock.children[1].value;
-    //         var newEventHourId = buttonInit.parentElement.id;
-    //         newEvent = {
-    //             hour : newEventHourId,
-    //             event : newEventInput
-    //         }
-    //         localStorage.setItem('agendaEvents', JSON.stringify(newEvent));
-    //     });
-    // };
-    
-    saveButton9.addEventListener('click', function (e) {
-        saveEvent(saveButton9);
-    });
-
-    saveButton10.addEventListener('click', function (e) {
-        saveEvent(saveButton10);
-    });
-
-    saveButton11.addEventListener('click', function (e) {
-        saveEvent(saveButton11);
-    });
-
-    saveButton12.addEventListener('click', function (e) {
-        saveEvent(saveButton12);
-    });
-
-    saveButton13.addEventListener('click', function (e) {
-        saveEvent(saveButton13);
-    });
-
-    saveButton14.addEventListener('click', function (e) {
-        saveEvent(saveButton14);
-    });
-
-    saveButton15.addEventListener('click', function (e) {
-        saveEvent(saveButton15);
-    });
-
-    saveButton16.addEventListener('click', function (e) {
-        saveEvent(saveButton16);
-    });
-
-    saveButton17.addEventListener('click', function (e) {
-        saveEvent(saveButton17);
-    });
-
-    saveButton18.addEventListener('click', function (e) {
-        saveEvent(saveButton17);
-    });
-
-  // TODO: Add code to apply the past, present, or future class to each time
    // Continuously evalute the time every second to set the appropriate class - past, present, or future
   var updatehourClass = function() {
   var currentHour = Number(dayjs().format('HH'));
@@ -117,15 +59,18 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
   
-  // array with each hour id
-  function readEventsFromStorage() {
+
+
+
+  
+  var readEventsFromStorage = function () {
     agendaEvents = JSON.parse(localStorage.getItem('agendaEvents')) ?? [];
-    console.log(agendaEvents)
-  }
+    console.log(agendaEvents);
+  };
 
-  // TODO: Add code to display the current date in the header of the page.
-
+  // Displays the current date in the header of the page.
   var displayTime =function() {
     var today = dayjs().format('dddd, MMMM D, YYYY');
     $('#currentDay').text(today);
@@ -133,7 +78,47 @@ $(function () {
     $('#currentTime').text(currentTime);
   };
 
-
   setInterval(displayTime, 1000);
   setInterval(updatehourClass, 1000);
+
+  saveButton9.addEventListener('click', function (e) {
+    saveEvent(saveButton9);
+  });
+
+  saveButton10.addEventListener('click', function (e) {
+      saveEvent(saveButton10);
+  });
+
+  saveButton11.addEventListener('click', function (e) {
+      saveEvent(saveButton11);
+  });
+
+  saveButton12.addEventListener('click', function (e) {
+      saveEvent(saveButton12);
+  });
+
+  saveButton13.addEventListener('click', function (e) {
+      saveEvent(saveButton13);
+  });
+
+  saveButton14.addEventListener('click', function (e) {
+      saveEvent(saveButton14);
+  });
+
+  saveButton15.addEventListener('click', function (e) {
+      saveEvent(saveButton15);
+  });
+
+  saveButton16.addEventListener('click', function (e) {
+      saveEvent(saveButton16);
+  });
+
+  saveButton17.addEventListener('click', function (e) {
+      saveEvent(saveButton17);
+  });
+
+  saveButton18.addEventListener('click', function (e) {
+      saveEvent(saveButton17);
+  });
+
 });
