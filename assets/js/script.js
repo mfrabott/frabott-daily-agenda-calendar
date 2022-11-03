ad54931e3e5b6e43400e25340a8c31027c66a348
mfrabott <mfrabott14@gmail.com>
2022-11-02 22:17:33 -0400

    Add save event function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,50 +9,48 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //  
- 
-  var saveButton9 = document.getElementById("9");
-  var saveButton10 = document.getElementById("10");
-  var saveButton11 = document.getElementById("11");
-  var saveButton12 = document.getElementById("12");
-  var saveButton13 = document.getElementById("13");
-  var saveButton14 = document.getElementById("14");
-  var saveButton15 = document.getElementById("15");
-  var saveButton16 = document.getElementById("16");
-  var saveButton17 = document.getElementById("17");
-  var saveButton18 = document.getElementById("18");
-  var saveButton19 = document.getElementById("19");
-  var saveButton20 = document.getElementById("20");
-  var saveButton21 = document.getElementById("21");
-  var saveButton22 = document.getElementById("22");
+ 	var saveButton9 = document.getElementById("9");
+	var saveButton10 = document.getElementById("10");
+	var saveButton11 = document.getElementById("11");
+	var saveButton12 = document.getElementById("12");
+	var saveButton13 = document.getElementById("13");
+	var saveButton14 = document.getElementById("14");
+	var saveButton15 = document.getElementById("15");
+	var saveButton16 = document.getElementById("16");
+	var saveButton17 = document.getElementById("17");
+	var saveButton18 = document.getElementById("18");
+	var saveButton19 = document.getElementById("19");
+	var saveButton20 = document.getElementById("20");
+	var saveButton21 = document.getElementById("21");
+	var saveButton22 = document.getElementById("22");
 
-  var events = JSON.parse(localStorage.getItem('events')) ?? [];
+    var events = JSON.parse(localStorage.getItem('agendaEvents')) ?? [];
 
-   saveButton9.addEventListener('click', function (e) {
-    var hourBlock = this.parentElement
-    var newEventInput = hourBlock.children[1].value;
-    var newEventHourId = this.parentElement.id;
-    
-    newEvent = {
-      hour : newEventHourId,
-      event : newEventInput
-    }
-    
-    localStorage.setItem('events', JSON.stringify(newEvent))
-  });
+    var saveEvent = function(buttonNumber){
+        var hourBlock = buttonNumber.parentElement
+        var newEventInput = hourBlock.children[1].value;
+        var newEventHourId = buttonNumber.parentElement.id;
+        newEvent = {
+            hour : newEventHourId,
+            event : newEventInput
+        }
+        localStorage.setItem('agendaEvents', JSON.stringify(newEvent))
+    };
 
-  saveButton10.addEventListener('click', function (e) {
-    console.log(this.id)           // logs the className of my_element
-    console.log(this.parentElement.id) // logs `true`
-  });
+    // TODO: ARRAY AND FOR LOOP
 
-  saveButton11.addEventListener('click', function (e) {
-    console.log(this.id)           // logs the className of my_element
-    console.log(this.parentElement.id) // logs `true`
-  });
+    saveButton9.addEventListener('click', function (e) {
+        saveEvent(saveButton9);
+    });
 
-  // newEvent ={
+    saveButton10.addEventListener('click', function (e) {
+        saveEvent(saveButton10);
+    });
 
-  // }
+    saveButton11.addEventListener('click', function (e) {
+        saveEvent();
+    });
+
 
   // TODO: Add code to apply the past, present, or future class to each time
   
@@ -63,7 +61,12 @@ $(function () {
   // Continuously evalute the time every second to set the appropriate class - past, present, or future
 
   setInterval(function() {
-    for (i=0; i<agendaHoursEl.children().length; i++){
+    var today = dayjs().format('dddd, MMMM D, YYYY');
+  	$('#currentDay').text(today);
+  	var currentTime = dayjs().format('h:mm:ss A');
+  	$('#currentTime').text(currentTime);
+
+	for (i=0; i<agendaHoursEl.children().length; i++){
         
       hourCheck = agendaHoursEl.children().eq(i).attr('id').match(/\d+/)[0];
       if (hourCheck == currentHour){
@@ -85,10 +88,6 @@ $(function () {
 
 
   // TODO: Add code to display the current date in the header of the page.
-  var today = dayjs().format('dddd, MMMM D, YYYY');
-  $('#currentDay').text(today);
-  var currentTime = dayjs().format('h:mm A');
-  $('#currentTime').text(currentTime);
 
 
   
